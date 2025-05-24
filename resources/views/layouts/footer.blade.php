774ff0c5172737accfb15c607c4f9a026cac7f13
ThomasCortes <thomascastillo142@gmail.com>
2025-05-23 22:11:20 -0500

    agregue los iconos de las redes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,18 @@
 <footer>
   <div class="footer-container">
       <div class="footer-section">
-          <h3>Mantengace Informado</h3>
-          <form>
-              <input type="email" placeholder="Dirección de correo electrónico" required>
-              <button type="submit">Enviar</button>
-              <p>Al firmar y hacer clic en Enviar, confirma que ha leido y acepta la politica de privacidad y las condiciones de uso y que desea recibir noticias.</p>
-          </form>
+          <section id="contacto" class="contacto py-8 bg-gray-50">
+            <h2 class="text-3xl font-semibold mb-4 text-center text-gray-800">Redes</h2>
+
+            <nav class="flex flex-col items-center space-y-6" aria-label="Redes sociales">
+                <a href="#" aria-label="Facebook" class="transform hover:scale-110 transition-transform duration-300">
+                    <img src="images/facebook.png" alt="Facebook" class="w-16 h-16">
+                </a>
+                <a href="#" aria-label="Instagram" class="transform hover:scale-110 transition-transform duration-300">
+                    <img src="images/Instagram.png" alt="Instagram" class="w-16 h-16">
+                </a>
+                </nav>
+        </section>
       </div>
       <div class="footer-section">
           <h3>Contactanos</h3>
